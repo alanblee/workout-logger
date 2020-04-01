d667aeb4713086f4ba463ef940968b2acd41b378
alanblee <alanbenlee12@gmail.com>
2020-04-01 12:28:41 -0700

    added routes and some basic html structure for components

---
+++ client/src/components/workouts/main-workout-page.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./main-workout-page.scss";
+
+const WorkoutPage = () => {
+  //setState for all workouts
+  
+  return <div>all workouts will go here</div>;
+};
+
+export default WorkoutPage;
